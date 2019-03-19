0bf21fa3664a8be050992e1f9a3ec521cb2cb3aa
yyspyysp <45526192+yyspyysp@users.noreply.github.com>
2019-03-19 21:41:07 +0800

    增加垃圾生成

--- Assets/Scripts/RubbishManager.cs
+++ Assets/Scripts/RubbishManager.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class RubbishManager : MonoBehaviour {
+public class RubbishManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int rows = 10;
+    public int cols = 10;
+    public GameObject[] RubbishArray;
+    private List<Vector2> positionList = new List<Vector2>();
+    private Transform RubbishHolder;
+
+    public float CountDown = 10;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDown -= Time.deltaTime;
+        if (CountDown <=0)
+        {
+            CreateRubbish();
+            CountDown = 10;
+            return;
+        }
+    }
+
+    public void CreateRubbish()
+    {
+        int indexX = Random.Range(0, cols);
+        int indexY = Random.Range(0, rows);
+        int index = Random.Range(0, RubbishArray.Length);
+        GameObject go = GameObject.Instantiate(RubbishArray[index], new Vector3(indexX, indexY, 0), Quaternion.identity) as GameObject;
+        go.transform.SetParent(RubbishHolder);
+        Rigidbody rigidbody = go.GetComponent<Rigidbody>();
+        rigidbody.velocity=new Vector3(10,0,0);
+        Destroy(go.gameObject,8f);
+    }
 }
+
