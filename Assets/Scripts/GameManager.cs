531fbd5c1e7a2575e06c9efd264b45e21ce7283c
wangtianci <377090139@qq.com>
2019-03-21 19:56:07 +0800

    关卡切换

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,6 +25,8 @@ public class GameManager : MonoBehaviour {
 
     //文本
     public GameObject LoseText, WinText, RestartText;
+    //按钮
+    public GameObject LoseButton, RestartButton, NextLevelButton;
 
 	// Use this for initialization
 	void Awake () {
@@ -51,38 +53,31 @@ public class GameManager : MonoBehaviour {
         temp.SetActive(false);
         Time.timeScale = 1;
         SceneManager.LoadScene(1);
+
+        GotRubbishNum = 0;
+        CurrentLevel = 1;
     }
 
     //增加积分 每捡取一次垃圾就调用一次
    public void AddScore()
     {
         Score += SingleScore;
+        GotRubbishNum += 1;
     }
-
-
-    //游戏结束
-   public void GameOver(bool isdead)
-    {
-        if(isdead)
-        {
-
-        }
-    }
-
-
+    
     //失败
    public void Lose()
     {
-        //重来
-        GameRestart();
-        //退出
-        QuitGame();
+        LoseText.SetActive(true);
+        LoseButton.SetActive(true);
+        RestartButton.SetActive(true);
     }
 
     //重新开始
    public void GameRestart()
     {
         SceneManager.LoadScene(CurrentLevel);
+        isDead = false;
     }
 
     //退出
@@ -94,6 +89,16 @@ public class GameManager : MonoBehaviour {
     //胜利
    public void Win()
     {
+        WinText.SetActive(true);
+        if (CurrentLevel != MaxLevelNum)
+            NextLevelButton.SetActive(true);
+        else LoseText.SetActive(true);
+    }
+
+    //下一关
+    public void NextLevel()
+    {
+
         //若不是最后一关则跳转至下一关
         if (CurrentLevel != MaxLevelNum)
             SceneManager.LoadScene(++CurrentLevel);
@@ -102,6 +107,7 @@ public class GameManager : MonoBehaviour {
         {
             WinText.SetActive(true);
         }
+
     }
 
     //难度控制
