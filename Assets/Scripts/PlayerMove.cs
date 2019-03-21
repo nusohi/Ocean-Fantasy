c58f038c3c7a4c67f3016b5d733ba3fadebee561
nuso <nelohjb@hotmail.com>
2019-03-21 20:24:20 +0800

    Player移动朝向角度调整

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -71,5 +71,19 @@ public class PlayerMove : MonoBehaviour {
         if (Mathf.Abs(h) > 0.0001 || Mathf.Abs(v) > 0.0001) {
             rigidbody.transform.position += new Vector3(h * _Step_, v * _Step_, 0);
         }
+
+        // 人物朝向角度调整
+        float angle = Vector2.SignedAngle(new Vector2(1, 0), new Vector2(h, v));
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+        // 左右移动变换时修正上下
+        Vector3 scale = transform.localScale;
+        if (h < 0 && scale.y > 0) {
+            scale.y *= -1;
+        }
+        else if (h > 0 && scale.y < 0) {
+            scale.y *= -1;
+        }
+        transform.localScale = scale;
     }
 }
